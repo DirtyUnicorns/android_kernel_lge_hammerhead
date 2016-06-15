df8aed221141ac32f40d03ca18cba7a6e244756c
Abhijit Kulkarni <kabhijit@codeaurora.org>
2016-06-15 10:30:50 -0700

    msm: mdss: hide kernel addresses from unprevileged users
    for printing kernel pointers which should be hidden from unprivileged
    users, use %pK which evaluates whether kptr_restrict is set.
    
    CRs-Fixed: 987021
    Change-Id: Ie49eee9478f4657cfb2a994ba60da1ec4c356339
    Signed-off-by: Abhijit Kulkarni <kabhijit@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -322,7 +322,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
 
-	pr_debug("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_debug("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 	if (local_pdata->on_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &local_pdata->on_cmds);
@@ -354,7 +354,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	pr_debug("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_debug("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 	mipi  = &pdata->panel_info.mipi;
 
